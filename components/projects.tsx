0f25e2203079f4d4f60089210dfe0c67a68719ab
Pratikshit Singh <pratikshit.singh.eee16@itbhu.ac.in>
2026-03-15 09:58:01 -0700

    feat: fix for all resolutions

--- components/projects.tsx
+++ components/projects.tsx
@@ -11,7 +11,7 @@ export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.15);
 
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
